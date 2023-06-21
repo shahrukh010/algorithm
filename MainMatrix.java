06321d0c5fae03cf023da5ffdbe7920a0b03cf66
hector <khanshahrukhbsc@gmail.com>
2023-06-21 07:14:33 +0530

    matrix

--- MainMatrix.java
+++ MainMatrix.java
@@ -1,4 +1,6 @@
 import com.code.main.Matrix;
+import java.util.List;
+import java.util.Arrays;
 
 public class MainMatrix{
 
@@ -10,7 +12,21 @@ public class MainMatrix{
         int[][] mt = {{1,3,5,7},{10,11,16,20},{23,30,34,50}};
 
         boolean result = matrix.searchFromMatrix(mt,1);
-        System.out.println(result);
+ //       System.out.println(result);
         
+//        List<Integer> list = Arrays.asList(10,7,10,11,10,7,5,6);
+
+//        matrix.sortByFrequency(list);
+//        System.out.println(list);
+
+        int[][] input = {{1,3,6},{3,7,11}};
+
+       int[][] res =  matrix.computeBeforeMatrix(input.length,input[0].length,input);
+
+
+        for(int[]print : res){
+
+            System.out.println(java.util.Arrays.toString(print));
+        }
     }
 }
--- Matrix.java
+++ Matrix.java
@@ -1,7 +1,37 @@
 package com.code.main;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collections;
 
 public class Matrix{
 
+
+
+    public List<Integer> sortByFrequency(List<Integer> list){
+
+        Map<Integer,Integer> map = new HashMap();
+
+        list.forEach(value->{
+
+            map.put(value,map.getOrDefault(value,0)+1);
+        });
+
+        for(Map.Entry<Integer,Integer> m : map.entrySet()){
+
+            System.out.println(m.getKey()+":"+m.getValue());
+          }
+
+        Collections.sort(list,(n1,n2)->{
+
+            int f1 = map.get(n1);
+            int f2 = map.get(n2);
+
+            return f1 !=f2 ? f2 - f1 : n2 - n1;
+        });
+
+        return list;
+    }
 
     public boolean searchFromMatrix(int[][]input,int target){
 
@@ -27,6 +57,41 @@ public class Matrix{
         }
         return false;
     }
+
+
+    public int[][] computeBeforeMatrix(int n, int m, int[][]after){
+
+        int[][]before = new int[n][m];
+        before[0][0] = after[0][0];
+
+        for(int i = m -1; i >=1; i--){
+
+            before[0][i] = after[0][i] - after[0][i-1];
+        }
+
+        for(int i = n-1; i>=1; i--){
+
+            before[i][0] = after[i][0] - after[i-1][0];
+        }
+
+
+        for(int i = 1; i <n; i++){
+
+            for(int j = 1; j < m; j++){
+
+                int tmp = after[i-1][j] + after[i][j-1] - after[i-1][j-1];
+
+                before[i][j] = after[i][j] - tmp;
+            }
+
+        }
+        return before;
+    }
+
+
+
+
+
 
 }
 
